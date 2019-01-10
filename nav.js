c1d4e549e69ca2bbcdf182f38fe6f13bfbd314cc
Arif Waqas <arifwaqas@mail.com>
2019-01-10 03:37:41 +0300

    Deploy commit

--- nav.js
+++ nav.js
@@ -1,31 +1,8 @@
-let toggleNavStatus = false;
-
-let toggleNav = function(){
-	let getSidebar = document.querySelector(".nav-sidebar");
-	let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
-	let getSidebarUI = document.querySelector(".nav-sidebar ul");
-	let getLogo = document.querySelector(".btn-toggle-nav");
-
-	if (toggleNavStatus === false) {
-		getSidebarUI.style.visibility = "visible";
-		getSidebar.style.width="272px";
-		getLogo.style.opacity="0.8";
-		let arLength = getSidebarLinks.length;
-		for (let i = 0; i < arLength; i++){
-   			getSidebarLinks[i].style.opacity="1";
-		}
-		toggleNavStatus = true;
-	}
-
-	
-	else if (toggleNavStatus === true) {
-		getSidebarUI.style.visibility = "hidden";
-		getSidebar.style.width="50px";
-		getLogo.style.opacity="1";
-		let arLength = getSidebarLinks.length;
-		for (let i = 0; i < arLength; i++){
-   			getSidebarLinks[i].style.opacity="0";
-		}
-		toggleNavStatus = false;
-	}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
